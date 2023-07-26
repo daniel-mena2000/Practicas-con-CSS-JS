3123152e2d0ef7831819104f98cacfdc07269fa2
daniel mena <dmena044@gmail.com>
2023-07-25 22:00:29 -0600

    css

--- PROYECTO 2/main.js
+++ PROYECTO 2/main.js
@@ -1,52 +1,136 @@
 
 
-const inputCard = document.querySelector("#inputCard");
-const inputDate = document.querySelector("#inputDate");
-const inputCVV = document.querySelector("#inputCVV");
 
-//Mascaras, podemos poner cualquier simbolo solo para mas adelante identificarlos y sustituirlos
-const maskNumber = '####-####-####-####';
-const maskDate = '##/##';
-const maskCVV = '###';
+const inicio = document.querySelector(".btnInicio").disabled = true;
 
-//actual
-let current = "";
-//Para cada uno de los input creamos un arreglo en donde se alamcenaran los valores
-let cardNumber = [];
-let dateNumber = [];
-let cvvNumber = [];
-//Cuando presione la tecla
-inputCard.addEventListener("keydown", e => {
-    if (e.key === "Tab") {
-        return;
-    }
-    e.preventDefault();
-//Cuando yo presione una tecla voy a mandar llamar esta funcion
-//Esta funcion nos pedira lo siguiente, (e.key) seria la tecla que estamos presionando que solo aceptara numeros
-    handleInput(maskNumber, e.key, cardNumber);
-//Recordar que join es para unir
-    inputCard.value = cardNumber.join("");
-})
+const contar = document.querySelector(".contador")
+const fin = document.querySelector(".btnFinal").disabled = true;
+const inputTarea = document.querySelector(".ing_tarea");
 
-function handleInput(mask, key, arr){
-//Este array nos permite que solo estos caracteres pueda ingresar el usuario
-    let numbers = ['0', '1', '2', '3', '4','5','6','7','8','9'];
-//Validaremos si la tecla es de retroceso para borrar, y que el arreglo que le pasemos a ala funcion sea mayor que 0
-    if (key === 'Backspace' && arr.length > 0) {
-        arr.pop();
-        return;
-    }
-//Que pasa si no estamos presionando la tecla de retroceso, pues ahora si necesita validar por las teclas, osea nuestro arreglo de numeros
-//Quiero que me valide si dentro de este arreglo existe el valor de "key" y ademas el arreglo cuando le saco la longitud le sumo 1 es menor o igual que mi mascara en su longitud, ejecute lo siguiente:
-    if(numbers.includes(key) && arr.length + 1 <= mask.length){
-//Ahora yo necesito saber si en mi mascara en la posicion de arr.length el caracter que tengo ahi es igual a mi signo (-) o (/) que son los caracteres que asignamos anteriormente
-        if (mask[arr.length] === "-" || mask[arr.length] === "/") {
-//Entonces a nuestro arreglo le agregamos el valor que hay en mascara en la posicion arr.length y despues la tecla
-            arr.push(mask[arr.length], key);
-        }else{
-//Si lo anterior no es verdadero entonces solo agrego mi tecla pulsada
-            arr.push(key);
+const mensaje = document.querySelector(".mensaje_err");
+
+
+let segundos = 0;
+let minutos = 0;
+
+//Para mostrar los segundos como si fueran un reloj, puedes utilizar la función padStart() 
+function formatTime(time) {
+    return time.toString().padStart(2, "0");
+  }
+  
+
+function iniciaEstudio(){
+    let alarmaTempo;
+    const alarma = document.createElement("audio")
+    alarma.src = "/img/alarma.mp3";
+
+    document.addEventListener("keyup", e =>{
+        if (e.target.matches(".ing_tarea")) {
+            document.querySelector(".btnInicio").disabled = false;
+
+            if (inputTarea.value == null || inputTarea.value == "") {
+                document.querySelector(".btnInicio").disabled = true;
+            }
         }
-    }
+    })
+
+    let time;
+    document.addEventListener("click", e => {
+        if (e.target.matches(".btnInicio")) {
+
+            if (inputTarea.value !== "" ) {
+
+                const divConteiner = document.createElement("div");
+                divConteiner.classList.add("contenedor_inp");
+
+                const p = document.createElement("p");
+                p.classList.add("texto_tarea")
+                p.textContent = inputTarea.value
+
+                const botonDel = document.createElement("button");
+                botonDel.textContent = "❌";
+                botonDel.classList.add("delet_Boton")
+
+                const contenedorTareas = document.querySelector(".tareas_guardadas");
+                contenedorTareas.appendChild(divConteiner)
+
+                divConteiner.appendChild(p);
+                divConteiner.appendChild(botonDel);
+
+
+                document.querySelector(".ing_tarea").value = "";
+                document.querySelector(".btnFinal").disabled = false;
+
+            }
+            time = setInterval(() => {
+
+                let cont = ++segundos;
+                let formattedSeconds = formatTime(segundos);
+                let formattedMinutes = formatTime(minutos);
+                contar.innerHTML = `<h4>${formattedMinutes}:${formattedSeconds}</h4>`;
+               
+                if (segundos >= 9) {
+                    segundos = -1;
+                    minutos++;
+                    if (minutos === 2){
+                        alarmaTempo = setTimeout(() => {
+                            alarma.play()
+                        }, 1000);
+                       clearInterval(time) 
+                       contar.innerHTML = `<h4>Fin de tu tiempo de estudio :)<h4/>`
+
+                       minutos = 0;
+                       segundos = 0;
+                       return
+
+                    }
+                }
+                renderTime();
+
+            }, 1000);
+            e.target.disabled = true;
+        }
+        if (e.target.matches(".btnFinal")){
+            clearInterval(time) 
+            contar.innerHTML = '';
+            minutos = 0;
+            segundos = 0;
+
+        }
+        if (e.target.matches(".btnParar")) {
+            alarma.pause()
+
+        }
+        if (e.target.matches(".delet_Boton")) {
+            const container = e.target.closest(".contenedor_inp"); // Obtener el contenedor padre del botón de eliminación
+            container.remove(); // Eliminar el contenedor completo
+            clearInterval(time) 
+            contar.innerHTML = '';
+            minutos = 0;
+            segundos = 0;
+            if (inputTarea.textContent !== "") {
+                console.log("hola");
+            }
+        }
+        if((e.target.matches(".ing_tarea") && contar.innerHTML !== "")){
+            document.querySelector(".btnInicio").disabled = true;
+            document.querySelector(".ing_tarea").disabled = true;
+            mensaje.style.display = "block";
+          setTimeout(() => {
+            mensaje.style.display = "none";
+          }, 4000);
+
+        }else{
+            document.querySelector(".ing_tarea").disabled = false;
+            mensaje.style.display = "none";
+
+
+        }
+    })
+
+
 }
+
+document.addEventListener("DOMContentLoaded", iniciaEstudio);
+
 
